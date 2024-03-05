7201844726ae0a068d4b552156a4f7b39a0137ed
achoruzy <achoruzy@gmail.com>
2024-03-05 21:17:41 +0100

    fixed most of issues with generation script

--- script.py
+++ script.py
@@ -1,11 +1,20 @@
 import bpy
 
-export_coll = bpy.data.collections.new("EXPORT")
+export_coll = bpy.data.collections.new("EXPORT_OBJECTS")
 bpy.context.scene.collection.children.link(export_coll)
 
 scene = bpy.context.scene
+obj_list = scene.objects
 
-for obj in scene.objects:
+for obj in obj_list:
+    
+    obj_copy = obj.copy()
+    obj_copy.data = obj.data.copy()
+    obj_copy.parent = None
+    obj_copy.name = f'SM_{obj.name}'
+    
+    export_coll.objects.link(obj_copy)
+        
     if obj.children:
         for child in obj.children:
             bpy.ops.object.empty_add(
@@ -14,23 +23,11 @@ for obj in scene.objects:
                 scale = child.matrix_local.to_scale())
             empty = bpy.context.active_object
             empty.name = f'Socket_{child.name}'
-            empty.parent = obj
-            
+            empty.parent = obj_copy
 
-for obj in scene.objects:
-    if obj.type != 'EMPTY':
-        bpy.context.view_layer.objects.active = obj
-        bpy.ops.object.duplicate()
-        print(obj.name)
-        # export_coll.objects.link(obj_copy)
-        # bpy.ops.object.move_to_collection(collection_index=len(bpy.data.collections.values()))
+            bpy.context.scene.collection.objects.unlink(empty)
+            export_coll.objects.link(empty)
         
-        # if obj.children:
-        #     for child in obj.children:
-        #         if child.type == 'EMPTY':
-        #             child_copy = child.copy()
-        #             child.parent = obj_copy
-        
-        # bpy.context.active_object.location = (0, 0, 0)
-        # bpy.context.active_object.rotation_euler = (0, 0, 0)
+    obj_copy.location = (0, 0, 0)
+    obj_copy.rotation_euler = (0, 0, 0)
             
