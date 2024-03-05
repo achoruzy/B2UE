a53c0c853f1c60ce484ded559c69c82eb3d46a28
achoruzy <achoruzy@gmail.com>
2024-03-05 20:20:33 +0100

    WIP script

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
 import bpy
 
+export_coll = bpy.data.collections.new("EXPORT")
+bpy.context.scene.collection.children.link(export_coll)
+
 scene = bpy.context.scene
 
 for obj in scene.objects:
@@ -13,20 +16,21 @@ for obj in scene.objects:
             empty.name = f'Socket_{child.name}'
             empty.parent = obj
             
-export_coll = bpy.data.collections.new("EXPORT")
-bpy.context.scene.collection.children.link(export_coll)
 
 for obj in scene.objects:
     if obj.type != 'EMPTY':
-        obj_copy = obj.copy()
+        bpy.context.view_layer.objects.active = obj
+        bpy.ops.object.duplicate()
+        print(obj.name)
         # export_coll.objects.link(obj_copy)
-        bpy.ops.object.move_to_collection(collection_index=len(bpy.data.collections.values()))
+        # bpy.ops.object.move_to_collection(collection_index=len(bpy.data.collections.values()))
         
-        if obj.children:
-            for child in obj.children:
-                child_copy = child.copy()
-                child_copy.parent = obj_copy
+        # if obj.children:
+        #     for child in obj.children:
+        #         if child.type == 'EMPTY':
+        #             child_copy = child.copy()
+        #             child.parent = obj_copy
         
-        obj_copy.location = (0, 0, 0)
-        obj_copy.rotation_euler = (0, 0, 0)
+        # bpy.context.active_object.location = (0, 0, 0)
+        # bpy.context.active_object.rotation_euler = (0, 0, 0)
             
