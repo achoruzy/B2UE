73763a6db9d24fa81cfe47e407cd70439a6d57c2
achoruzy <achoruzy@gmail.com>
2024-03-16 20:52:19 +0100

    updated with script concept

---
+++ ue_scripts/assemble_sockets_to_bp.py
@@ -0,0 +1,20 @@
+import unreal
+
+
+def create_bp():
+    ...
+    
+def find_model_parts():
+    ...
+    
+def assemble_model():
+    ...
+
+def register_menu_position():
+    ...
+
+def run_script():
+    register_menu_position()
+
+if __name__ == "__main__":
+    run_script()
