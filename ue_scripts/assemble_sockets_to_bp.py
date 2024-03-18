8a72d41eaa2053acf60b342fb16f888e4a051e3f
achoruzy <achoruzy@gmail.com>
2024-03-18 20:48:16 +0100

    implemented registering menu

--- ue_scripts/assemble_sockets_to_bp.py
+++ ue_scripts/assemble_sockets_to_bp.py
@@ -1,17 +1,35 @@
 import unreal
 
+@unreal.uclass()
+class ScriptObject(unreal.ToolMenuEntryScript):
+    
+    @unreal.ufunction(override=True)
+    def execute(self, context: unreal.ToolMenuContext) -> None:
+        unreal.log_warning("Not yet implemented!")
 
-def create_bp():
-    ...
-    
-def find_model_parts():
-    ...
-    
-def assemble_model():
-    ...
+    def create_bp():
+        ...
+        
+    def find_model_parts():
+        ...
+        
+    def assemble_model():
+        ...
 
 def register_menu_position():
-    ...
+    menus = unreal.ToolMenus().get()
+    mesh_context_menu = menus.find_menu("ContentBrowser.AssetContextMenu.StaticMesh")
+    
+    script = ScriptObject()
+    script.init_entry(
+        owner_name = mesh_context_menu.menu_name,
+        menu = mesh_context_menu.menu_name,
+        section = "GetAssetActions",
+        name = "AC_CreateAssemblyBP",
+        label = "Create assembly blueprint",
+        tool_tip = "Creates new blueprint with this mesh and all found socket parts recursively."
+    )
+    script.register_menu_entry()
 
 def run_script():
     register_menu_position()
