6285eff7fc53701d8f9a69c2941d2e92175db4b8
achoruzy <achoruzy@gmail.com>
2024-03-20 21:08:11 +0100

    update

--- ue_scripts/assemble_sockets_to_bp.py
+++ ue_scripts/assemble_sockets_to_bp.py
@@ -1,19 +1,37 @@
 import unreal
 
 @unreal.uclass()
+class GetEditorUtility(unreal.GlobalEditorUtilityBase):
+    pass
+
+@unreal.uclass()
 class ScriptObject(unreal.ToolMenuEntryScript):
     
     @unreal.ufunction(override=True)
     def execute(self, context: unreal.ToolMenuContext) -> None:
-        unreal.log_warning("Not yet implemented!")
+        unreal.log_warning("Not yet implemented!!!")
+        # mesh: unreal.StaticMesh = context.find_by_class(unreal.StaticMesh)
+        # unreal.log(unreal.ContentBrowserAssetContextMenuContext().selected_assets)
+        
+        editorUtility = GetEditorUtility()
+        unreal.log(editorUtility.get_selected_assets())
+        self.create_bp()
+        
 
-    def create_bp():
+    def create_bp(self):
+        asset_tools = unreal.AssetToolsHelpers.get_asset_tools()
+        
+        factory = unreal.BlueprintFactory()
+        factory.set_editor_property("Actor", unreal.Actor)
+        
+        bp_actor = asset_tools.create_asset_with_dialog("BP_", "./", None, factory)
+        unreal.EditorAssetLibrary.save_loaded_asset(bp_actor)
+        
+        
+    def find_model_parts(self):
         ...
         
-    def find_model_parts():
-        ...
-        
-    def assemble_model():
+    def assemble_model(self):
         ...
 
 def register_menu_position():
