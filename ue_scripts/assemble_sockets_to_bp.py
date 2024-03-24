f27eb160330589316d2d9dd2b644e68096dfa182
achoruzy <achoruzy@gmail.com>
2024-03-24 20:44:15 +0100

    wip update

--- ue_scripts/assemble_sockets_to_bp.py
+++ ue_scripts/assemble_sockets_to_bp.py
@@ -8,25 +8,58 @@ class GetEditorUtility(unreal.GlobalEditorUtilityBase):
 class ScriptObject(unreal.ToolMenuEntryScript):
     
     @unreal.ufunction(override=True)
-    def execute(self, context: unreal.ToolMenuContext) -> None:
-        unreal.log_warning("Not yet implemented!!!")
-        # mesh: unreal.StaticMesh = context.find_by_class(unreal.StaticMesh)
-        # unreal.log(unreal.ContentBrowserAssetContextMenuContext().selected_assets)
+    def execute(self, context: unreal.ToolMenuContext) -> None:      
+        # 1. find base mesh, create bp and set mesh as component of actor
+        base_mesh = self.find_base_mesh()
+        if not base_mesh: return
         
+        bp_name = f"BP_{base_mesh.get_full_name().removeprefix('SM_').removesuffix('_base')}"
+        actor = self.create_bp(bp_name, base_mesh.get_path_name())
+        path = actor.get_folder_path()
+        
+        so_subsystem = unreal.get_engine_subsystem(unreal.SubobjectDataSubsystem)
+        root_sub_object = so_subsystem.k2_gather_subobject_data_for_instance(actor)[0]
+        new_sub_object: unreal.StaticMesh = so_subsystem.add_new_subobject(unreal.AddNewSubobjectParams(
+            parent_handle=root_sub_object,
+            new_class=unreal.StaticMesh,
+            ))
+        
+        # 2. get sockets
+        sockets = new_sub_object.get_sockets_by_tag('')
+        
+        # 3. check for meshes to put in sockets in same localization
+        editorUtility = GetEditorUtility();
+        
+        for socket in sockets:
+            socket: unreal.StaticMeshSocket
+            socket_name = socket.socket_name
+            
+            # editorUtility...
+            
+            
+        
+        # 4. repeat for each subcomponent
+        
+    def find_base_mesh(self) -> unreal.StaticMesh | None:
         editorUtility = GetEditorUtility()
-        unreal.log(editorUtility.get_selected_assets())
-        self.create_bp()
-        
+        selection = editorUtility.get_selected_assets()
+        unreal.log(selection)
+        for asset in selection:
+            asset: unreal.StaticMesh
+            if asset.get_full_name().endswith('_base'):
+                return asset
+        return None
 
-    def create_bp(self):
+    def create_bp(self, name: str, path: str) -> unreal.Actor:
         asset_tools = unreal.AssetToolsHelpers.get_asset_tools()
         
         factory = unreal.BlueprintFactory()
         factory.set_editor_property("Actor", unreal.Actor)
         
-        bp_actor = asset_tools.create_asset_with_dialog("BP_", "./", None, factory)
+        bp_actor = asset_tools.create_asset_with_dialog(name, path, None, factory)
         unreal.EditorAssetLibrary.save_loaded_asset(bp_actor)
-        
+        return bp_actor
+    
         
     def find_model_parts(self):
         ...
