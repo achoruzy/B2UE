dee82e6d4715cc8956c0fbaae3d8c01311f2475a
achoruzy <achoruzy@gmail.com>
2024-03-31 21:29:50 +0200

    implemented submesh adding to bp under parent component

--- ue_scripts/assemble_sockets_to_bp.py
+++ ue_scripts/assemble_sockets_to_bp.py
@@ -3,6 +3,7 @@
 
 import unreal
 from pathlib import Path
+from time import sleep
 
 @unreal.uclass()
 class GetEditorUtility(unreal.GlobalEditorUtilityBase):
@@ -12,7 +13,7 @@ class GetEditorUtility(unreal.GlobalEditorUtilityBase):
 class ScriptObject(unreal.ToolMenuEntryScript):
     
     @unreal.ufunction(override=True)
-    def execute(self, context: unreal.ToolMenuContext) -> None:      
+    def execute(self, context: unreal.ToolMenuContext) -> None:
         # 1. find base mesh, create bp and set mesh as component of actor
         base_mesh = self.find_base_mesh()
         if not base_mesh: return
@@ -22,21 +23,21 @@ class ScriptObject(unreal.ToolMenuEntryScript):
 
         actor = self.create_actor_bp(bp_name, bp_path)
         
-        subsystem = unreal.get_engine_subsystem(unreal.SubobjectDataSubsystem)
-        root_sub_object = subsystem.k2_gather_subobject_data_for_blueprint(actor)[0]
-        subobject_handle, fail_reason = subsystem.add_new_subobject(
+        SDS = unreal.get_engine_subsystem(unreal.SubobjectDataSubsystem)
+        root_handle = SDS.k2_gather_subobject_data_for_blueprint(actor)[0]
+        subobject_handle, fail_reason = SDS.add_new_subobject(
             unreal.AddNewSubobjectParams(
-                parent_handle = root_sub_object,
+                parent_handle = root_handle,
                 new_class = unreal.StaticMeshComponent,
                 blueprint_context = actor
                 )
             )
         print(fail_reason)
         
-        subsystem.rename_subobject(subobject_handle, base_mesh.get_name())
+        SDS.rename_subobject(subobject_handle, base_mesh.get_name())
         
-        bp_func_lib = unreal.SubobjectDataBlueprintFunctionLibrary
-        subobject: unreal.StaticMeshComponent = bp_func_lib.get_object(bp_func_lib.get_data(subobject_handle)) # this is the component finally
+        SDBPFL = unreal.SubobjectDataBlueprintFunctionLibrary
+        subobject: unreal.StaticMeshComponent = SDBPFL.get_object(SDBPFL.get_data(subobject_handle)) # this is the component finally
         subobject.set_static_mesh(base_mesh)
         
         # 2. get sockets
@@ -70,6 +71,53 @@ class ScriptObject(unreal.ToolMenuEntryScript):
                 print("xxxxxxxxxxx>>>>", mesh_to_assemble.get_name())
                 
                 # attach new component here
+                if mesh_to_assemble:
+                    submesh_handle, mesh_fail_reason = SDS.add_new_subobject(
+                        unreal.AddNewSubobjectParams(
+                            parent_handle = subobject_handle,
+                            new_class = unreal.StaticMeshComponent,
+                            blueprint_context = actor
+                            )
+                        )
+                    
+                    # unreal.BlueprintEditorLibrary.refresh_open_editors_for_blueprint(actor)
+                    
+                    SDS.rename_subobject(submesh_handle, mesh_to_assemble.get_name())
+                    submesh: unreal.StaticMeshComponent = SDBPFL.get_object(SDBPFL.get_data(submesh_handle))
+                    submesh.set_static_mesh(mesh_to_assemble)
+                                       
+                    unreal.BlueprintEditorLibrary.compile_blueprint(actor)
+                    # SDS.attach_subobject(
+                    #     owner_handle=subobject_handle,
+                    #     child_to_add_handle=submesh_handle
+                    # )
+                    
+                    help = submesh.k2_attach_to(
+                        parent=unreal.SceneComponent.cast(subobject),
+                        socket_name=socket.socket_name,
+                        attach_type=unreal.AttachLocation.SNAP_TO_TARGET,
+                        weld_simulated_bodies=False
+                    )
+                    # CHECK
+                    print('>>> Is socket exist> >>>', unreal.SceneComponent.cast(subobject).does_socket_exist(socket.socket_name), help)
+                    
+                    # submesh.attach_to_component( # seems to work but submesh has no given socket to use and is not properly located
+                    #     parent=subobject, #unreal.SceneComponent.cast(subobject),
+                    #     socket_name=socket.socket_name,
+                    #     location_rule=unreal.AttachmentRule.SNAP_TO_TARGET,
+                    #     rotation_rule=unreal.AttachmentRule.SNAP_TO_TARGET,
+                    #     scale_rule=unreal.AttachmentRule.SNAP_TO_TARGET
+                    # )
+                    
+                    print('>>> attached socket name >>>', submesh.get_attach_socket_name())
+                    
+                    # submesh.set_relative_transform(
+                    #     new_transform=unreal.Transform(),
+                    #     sweep=False,
+                    #     teleport=False
+                    #     )
+                    
+                    
                 
         # 4. repeat for each subcomponent
         
