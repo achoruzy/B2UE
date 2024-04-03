c05b3a2ed669c166d0ee2dff2f1f01634f52e618
achoruzy <achoruzy@gmail.com>
2024-04-03 21:06:06 +0200

    refactored script code to static class utils

--- ue_scripts/assemble_sockets_to_bp.py
+++ ue_scripts/assemble_sockets_to_bp.py
@@ -3,65 +3,93 @@
 
 import unreal
 from pathlib import Path
-from time import sleep
+
+class ScriptUtils():
+    @staticmethod
+    def get_base_mesh_from_selected() -> unreal.StaticMesh:
+        editorUtility = GetEditorUtility()
+        selection = editorUtility.get_selected_assets()
+        for asset in selection:
+            asset: unreal.StaticMesh
+            if asset.get_full_name().endswith('_base'):
+                return asset
+        return None
+    
+    @staticmethod
+    def create_BP_Actor(name: str, path: str) -> unreal.Actor:
+        asset_tools = unreal.AssetToolsHelpers.get_asset_tools()
+        
+        factory = unreal.BlueprintFactory()
+        factory.set_editor_property('ParentClass', unreal.Actor)
+        
+        bp_actor = asset_tools.create_asset(name, path, None, factory)
+        unreal.EditorAssetLibrary.save_loaded_asset(bp_actor)
+        return bp_actor
+    
+    @staticmethod
+    def add_BP_component(bp: unreal.Blueprint, comp_name: str, comp_type: unreal.SceneComponent, 
+                         parent_to: unreal.SubobjectDataHandle = None) -> tuple[unreal.SubobjectDataHandle, unreal.SceneComponent]:
+        # handlers
+        SDS = unreal.get_engine_subsystem(unreal.SubobjectDataSubsystem)
+        SDBPFL = unreal.SubobjectDataBlueprintFunctionLibrary
+        
+        if not parent_to:
+            parent_to = SDS.k2_gather_subobject_data_for_blueprint(bp)[0]
+            
+        subobject_handle, _fail_reason = SDS.add_new_subobject(
+            unreal.AddNewSubobjectParams(
+                parent_handle = parent_to,
+                new_class = comp_type,
+                blueprint_context = bp
+            )
+        )        
+        subobject = SDBPFL.get_object(SDBPFL.get_data(subobject_handle))
+        SDS.rename_subobject(subobject_handle, comp_name)
+        
+        return subobject_handle, subobject
+    
+    @staticmethod
+    def get_static_mesh_sockets(static_mesh: unreal.StaticMeshComponent) -> list[unreal.StaticMeshSocket]:
+        sockets = static_mesh.get_sockets_by_tag('')
+        return sockets
 
 @unreal.uclass()
 class GetEditorUtility(unreal.GlobalEditorUtilityBase):
     pass
 
 @unreal.uclass()
-class ScriptObject(unreal.ToolMenuEntryScript):
+class SocketAssemblerScript(unreal.ToolMenuEntryScript):
     
     @unreal.ufunction(override=True)
     def execute(self, context: unreal.ToolMenuContext) -> None:
-        # 1. find base mesh, create bp and set mesh as component of actor
-        base_mesh = self.find_base_mesh()
+        base_mesh = ScriptUtils.get_base_mesh_from_selected()
         if not base_mesh: return
         
         bp_name = f"BP_{base_mesh.get_name().removeprefix('SM_').removesuffix('_base')}"
         bp_path = Path(base_mesh.get_path_name()).parent.as_posix()
 
-        actor = self.create_actor_bp(bp_name, bp_path)
+        actor = ScriptUtils.create_BP_Actor(bp_name, bp_path)
+                    
+        basepart_handle, basepart = ScriptUtils.add_BP_component(
+            bp=actor,
+            comp_name=base_mesh.get_name(),
+            comp_type=unreal.StaticMeshComponent,
+        )
+        basepart.set_static_mesh(base_mesh)        
+        basepart_sockets = ScriptUtils.get_static_mesh_sockets(base_mesh)
+        
+        #### Refactored till here
         
         SDS = unreal.get_engine_subsystem(unreal.SubobjectDataSubsystem)
-        root_handle = SDS.k2_gather_subobject_data_for_blueprint(actor)[0]
-        subobject_handle, fail_reason = SDS.add_new_subobject(
-            unreal.AddNewSubobjectParams(
-                parent_handle = root_handle,
-                new_class = unreal.StaticMeshComponent,
-                blueprint_context = actor
-                )
-            )
-        print(fail_reason)
-        
-        SDS.rename_subobject(subobject_handle, base_mesh.get_name())
-        
         SDBPFL = unreal.SubobjectDataBlueprintFunctionLibrary
-        subobject: unreal.StaticMeshComponent = SDBPFL.get_object(SDBPFL.get_data(subobject_handle)) # this is the component finally
-        subobject.set_static_mesh(base_mesh)
-        
-        # 2. get sockets
-        print('2. get sockets')
-        sockets = base_mesh.get_sockets_by_tag('') # OK!
-        # sockets = [s.socket_name.replace('.', '_') for s in sockets] # as asset names have auto replace in names
-        
-        # 3. check for meshes to put in sockets in same localization
         EAL = unreal.EditorAssetLibrary
         PT = unreal.PackageTools
         
-        # LogPackageName: Warning: TryConvertFilenameToLongPackageName 
-        # was passed an ObjectPath (/Game/GAME/Props/Guns/AK47/Meshes/SM_AK_SM_AK_base.SM_AK_SM_AK_base) 
-        # rather than a PackageName or FilePath; it will be converted to the PackageName. 
-        # Accepting ObjectPaths is deprecated behavior and will be removed in a future release; 
-        # TryConvertFilenameToLongPackageName will fail on ObjectPaths.
-
         assets_in_dir = EAL.list_assets(directory_path=bp_path, recursive=False)
-        assets_in_dir = [PT.filename_to_package_name(a.removesuffix(a[a.rfind('.'):])) for a in assets_in_dir] # to fix the warning given above
-        for i in assets_in_dir:
-            print("===========>>>>>>>>>", i)
+        assets_in_dir = [PT.filename_to_package_name(a.removesuffix(a[a.rfind('.'):])) for a in assets_in_dir]
         
-        if sockets:
-            for socket in sockets:
+        if basepart_sockets:
+            for socket in basepart_sockets:
                 socket: unreal.StaticMeshSocket
                 socket_name = str(socket.socket_name).replace('.', '_')
                 print('>>>>>> socket name', socket_name)
@@ -74,33 +102,26 @@ class ScriptObject(unreal.ToolMenuEntryScript):
                 if mesh_to_assemble:
                     submesh_handle, mesh_fail_reason = SDS.add_new_subobject(
                         unreal.AddNewSubobjectParams(
-                            parent_handle = subobject_handle,
+                            parent_handle = basepart_handle,
                             new_class = unreal.StaticMeshComponent,
                             blueprint_context = actor
                             )
                         )
                     
+                    
                     # unreal.BlueprintEditorLibrary.refresh_open_editors_for_blueprint(actor)
                     
                     SDS.rename_subobject(submesh_handle, mesh_to_assemble.get_name())
                     submesh: unreal.StaticMeshComponent = SDBPFL.get_object(SDBPFL.get_data(submesh_handle))
-                    submesh.set_static_mesh(mesh_to_assemble)
-                                       
-                    unreal.BlueprintEditorLibrary.compile_blueprint(actor)
-                    # SDS.attach_subobject(
-                    #     owner_handle=subobject_handle,
-                    #     child_to_add_handle=submesh_handle
-                    # )
                     
-                    help = submesh.k2_attach_to(
-                        parent=unreal.SceneComponent.cast(subobject),
-                        socket_name=socket.socket_name,
-                        attach_type=unreal.AttachLocation.SNAP_TO_TARGET,
-                        weld_simulated_bodies=False
-                    )
-                    # CHECK
-                    print('>>> Is socket exist> >>>', unreal.SceneComponent.cast(subobject).does_socket_exist(socket.socket_name), help)
-                    
+                    submesh.call_method("K2_AttachToComponent", (
+                        basepart, 
+                        socket.socket_name, 
+                        unreal.AttachmentRule.SNAP_TO_TARGET,
+                        unreal.AttachmentRule.SNAP_TO_TARGET,
+                        unreal.AttachmentRule.SNAP_TO_TARGET,
+                        False
+                        ))
                     # submesh.attach_to_component( # seems to work but submesh has no given socket to use and is not properly located
                     #     parent=subobject, #unreal.SceneComponent.cast(subobject),
                     #     socket_name=socket.socket_name,
@@ -109,6 +130,27 @@ class ScriptObject(unreal.ToolMenuEntryScript):
                     #     scale_rule=unreal.AttachmentRule.SNAP_TO_TARGET
                     # )
                     
+                    submesh.set_static_mesh(mesh_to_assemble)
+                                       
+                    # submesh.set_editor_property("AttachSocketName", socket_name)
+                    # world = unreal.get_editor_subsystem(unreal.UnrealEditorSubsystem).get_editor_world()
+                    # submesh.call_method("SetAttachSocketName", (socket.socket_name, world,))
+                    
+                    # unreal.BlueprintEditorLibrary.compile_blueprint(actor)
+                    # SDS.attach_subobject(
+                    #     owner_handle=subobject_handle,
+                    #     child_to_add_handle=submesh_handle
+                    # )
+                    
+                    # help = submesh.k2_attach_to(
+                    #     parent=unreal.SceneComponent.cast(subobject),
+                    #     socket_name=socket.socket_name,
+                    #     attach_type=unreal.AttachLocation.SNAP_TO_TARGET,
+                    #     weld_simulated_bodies=False
+                    # )
+                    # CHECK
+                    print('>>> Is socket exist> >>>', unreal.SceneComponent.cast(basepart).does_socket_exist(socket.socket_name), help)
+                    
                     print('>>> attached socket name >>>', submesh.get_attach_socket_name())
                     
                     # submesh.set_relative_transform(
@@ -116,37 +158,15 @@ class ScriptObject(unreal.ToolMenuEntryScript):
                     #     sweep=False,
                     #     teleport=False
                     #     )
-                    
-                    
-                
+                                    
         # 4. repeat for each subcomponent
-        
-    def find_base_mesh(self) -> unreal.StaticMesh:
-        editorUtility = GetEditorUtility()
-        selection = editorUtility.get_selected_assets()
-        unreal.log(selection)
-        for asset in selection:
-            asset: unreal.StaticMesh
-            if asset.get_full_name().endswith('_base'):
-                return asset
-        return None
-
-    def create_actor_bp(self, name: str, path: str) -> unreal.Actor:
-        asset_tools = unreal.AssetToolsHelpers.get_asset_tools()
-        
-        factory = unreal.BlueprintFactory()
-        factory.set_editor_property('ParentClass', unreal.Actor)
-        
-        bp_actor = asset_tools.create_asset(name, path, None, factory)
-        unreal.EditorAssetLibrary.save_loaded_asset(bp_actor)
-        return bp_actor
     
 
 def register_menu_position():
     menus = unreal.ToolMenus().get()
     mesh_context_menu = menus.find_menu("ContentBrowser.AssetContextMenu.StaticMesh")
     
-    script = ScriptObject()
+    script = SocketAssemblerScript()
     script.init_entry(
         owner_name = mesh_context_menu.menu_name,
         menu = mesh_context_menu.menu_name,
